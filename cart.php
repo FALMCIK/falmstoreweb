3ec47b381da761ba5e868aed7afd2013d2d4597f
cagdastopal <ctopal18@outlook.com>
2024-01-23 16:42:14 +0300

    f

--- cart.php
+++ cart.php
@@ -1,22 +1,32 @@
 <?php
 include 'config.php';
 session_start();
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+};
+
+if(isset($_GET['logout'])){
+   unset($user_id);
+   session_destroy();
+   header('location:login.php');
+};
 if(isset($_POST['update_cart'])){
-   $update_quantity = $_POST['cart_quantity'];
-   $update_id = $_POST['cart_id'];
-   mysqli_query($conn, "UPDATE `cart` SET quantity = '$update_quantity' WHERE id = '$update_id'") or die('query failed');
-   $message[] = 'cart quantity updated successfully!';
-}
-
-if(isset($_GET['remove'])){
-   $remove_id = $_GET['remove'];
-   mysqli_query($conn, "DELETE FROM `cart` WHERE id = '$remove_id'") or die('query failed');
-   header('location:index.html');
-}
-  
-if(isset($_GET['delete_all'])){
-   mysqli_query($conn, "DELETE FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
-   header('location:index.html');
-}
-
+    $update_quantity = $_POST['cart_quantity'];
+    $update_id = $_POST['cart_id'];
+    mysqli_query($conn, "UPDATE `cart` SET quantity = '$update_quantity' WHERE id = '$update_id'") or die('query failed');
+    $message[] = 'cart quantity updated successfully!';
+ }
+ 
+ if(isset($_GET['remove'])){
+    $remove_id = $_GET['remove'];
+    mysqli_query($conn, "DELETE FROM `cart` WHERE id = '$remove_id'") or die('query failed');
+    header('location:index.php');
+ }
+   
+ if(isset($_GET['delete_all'])){
+    mysqli_query($conn, "DELETE FROM `cart` WHERE user_id = '$user_id'") or die('query failed');
+    header('location:index.php');
+ }
 ?>
