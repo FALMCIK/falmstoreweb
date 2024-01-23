9249df33d56e719ad32307a337b355298aea4cf5
cagdastopal <ctopal18@outlook.com>
2024-01-23 16:47:03 +0300

    f

--- cart.php
+++ cart.php
@@ -58,6 +58,7 @@ if(isset($_GET['delete_all'])){
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" href="son.css">
+   <link rel="stylesheet" href="css/style.css">
     <title>FALMSTORE</title>
 
 </head>
