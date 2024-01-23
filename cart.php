eddea0e1c6cf21b31c95c68015ecb8e954bab071
cagdastopal <ctopal18@outlook.com>
2024-01-23 17:22:57 +0300

    ff

--- cart.php
+++ cart.php
@@ -105,34 +105,7 @@ if(isset($message)){
 }
 ?>
 
-<div class="container">
 
-<div class="user-profile">
-
-   <?php
-      $select_user = mysqli_query($conn, "SELECT * FROM `user_form` WHERE id = '$user_id'") or die('query failed');
-      if(mysqli_num_rows($select_user) > 0){
-         $fetch_user = mysqli_fetch_assoc($select_user);
-      };
-   ?>
-
-   <p> username : <span><?php echo $fetch_user['name']; ?></span> </p>
-   <p> email : <span><?php echo $fetch_user['email']; ?></span> </p>
-   <div class="flex">
-      <a href="login.php" class="btn">login</a>
-      <a href="register.php" class="option-btn">register</a>
-      <a href="index.php?logout=<?php echo $user_id; ?>" onclick="return confirm('are your sure you want to logout?');" class="delete-btn">logout</a>
-   </div>
-
-</div>
-
-
-   
-   ?>
-
-   </div>
-
-</div>
 
 <div class="shopping-cart">
 
