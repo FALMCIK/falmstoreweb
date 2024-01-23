135b2bcd93087262d7bff6aea42bc39c7f91911d
cagdastopal <ctopal18@outlook.com>
2024-01-23 16:59:27 +0300

    f

--- cart.php
+++ cart.php
@@ -51,15 +51,17 @@ if(isset($_GET['delete_all'])){
 }
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
-   <link rel="stylesheet" href="style.css">
-    <title>FALMSTORE</title>
+   <title>shopping cart</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
@@ -71,23 +73,57 @@ if(isset($message)){
    }
 }
 ?>
-<header> 
-        <nav>
-            <ul id="menu">
-                <h1>FALMSTORE</h1>
-                <li><a href="index.html">Ana Sayfa</a></li>
-                <li><a href="products.html">Ürünler</a></li>
-                
-                <li><a href="cart.html">Sepet</a></li>
-                <li><a href="updates.html">Güncellemeler</a></li>
-                
-                
-                
-            </ul>
-        </nav>
-        
-    </header>   
 
+<div class="container">
+
+<div class="user-profile">
+
+   <?php
+      $select_user = mysqli_query($conn, "SELECT * FROM `user_form` WHERE id = '$user_id'") or die('query failed');
+      if(mysqli_num_rows($select_user) > 0){
+         $fetch_user = mysqli_fetch_assoc($select_user);
+      };
+   ?>
+
+   <p> username : <span><?php echo $fetch_user['name']; ?></span> </p>
+   <p> email : <span><?php echo $fetch_user['email']; ?></span> </p>
+   <div class="flex">
+      <a href="login.php" class="btn">login</a>
+      <a href="register.php" class="option-btn">register</a>
+      <a href="index.php?logout=<?php echo $user_id; ?>" onclick="return confirm('are your sure you want to logout?');" class="delete-btn">logout</a>
+   </div>
+
+</div>
+
+<div class="products">
+
+   <h1 class="heading">latest products</h1>
+
+   <div class="box-container">
+
+   <?php
+      $select_product = mysqli_query($conn, "SELECT * FROM `products`") or die('query failed');
+      if(mysqli_num_rows($select_product) > 0){
+         while($fetch_product = mysqli_fetch_assoc($select_product)){
+   ?>
+      <form method="post" class="box" action="">
+         <img src="images/<?php echo $fetch_product['image']; ?>" alt="">
+         <div class="name"><?php echo $fetch_product['name']; ?></div>
+         <div class="price">$<?php echo $fetch_product['price']; ?>/-</div>
+         <input type="number" min="1" name="product_quantity" value="1">
+         <input type="hidden" name="product_image" value="<?php echo $fetch_product['image']; ?>">
+         <input type="hidden" name="product_name" value="<?php echo $fetch_product['name']; ?>">
+         <input type="hidden" name="product_price" value="<?php echo $fetch_product['price']; ?>">
+         <input type="submit" value="add to cart" name="add_to_cart" class="btn">
+      </form>
+   <?php
+      };
+   };
+   ?>
+
+   </div>
+
+</div>
 
 <div class="shopping-cart">
 
