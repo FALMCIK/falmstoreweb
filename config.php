5992a3d4d81b45e0f8ae42de6c17f463146ecf2d
çağdaş topal <56447742+FALMCIK@users.noreply.github.com>
2024-05-16 14:50:32 +0300

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 $conn = mysqli_connect('localhost', 'root', 'F@lm3939', 'shop_db') or die('connection failed');
 
-// Bağlantı kontrolü
+// Bağlantı kontrolü 
 if (!$conn) {
     die("Veritabanına bağlantı hatası: " . mysqli_connect_error());
 }
