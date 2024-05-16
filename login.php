baf4b7d0a41d238d55ff33ccc2bea8c5d851361e
çağdaş topal <56447742+FALMCIK@users.noreply.github.com>
2024-05-16 14:51:15 +0300

    Update login.php

--- login.php
+++ login.php
@@ -7,7 +7,7 @@ if(isset($_POST['submit'])){
 
    $email = mysqli_real_escape_string($conn, $_POST['email']);
    $pass = mysqli_real_escape_string($conn, md5($_POST['password']));
-
+ 
    $select = mysqli_query($conn, "SELECT * FROM `user_form` WHERE email = '$email' AND password = '$pass'") or die('query failed');
 
    if(mysqli_num_rows($select) > 0){
